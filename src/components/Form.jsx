9f89ae46c9d38a9013f5cb9d2404cbca58cc8432
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-03-16 18:34:17 +0100

    feat: implement email submission form for waitlist

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,34 @@
-import '../styles/components/Form.scss';
+import { useState } from "react";
+import "../styles/components/Form.scss";
 
 const Form = () => {
+  const [email, setEmail] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const url = `https://script.google.com/macros/s/AKfycbxssXNYWFEcoBaLS-Y2V1hbL6ZK4QxTTOmT32SPgE7VZa4U67DQHzNp4wwpD-GzQnCO/exec?email=${email}`;
+    fetch(url, {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      // ignore status code as the url redirects but the data store still succeeds
+      setEmail("");
+      alert("You have been added to the waitlist!");
+    });
+  };
   return (
-    <div className="form">
-      <input className="form__input" placeholder="Enter your email address" />
+    <form onSubmit={onSubmit} className="form">
+      <input
+        className="form__input"
+        placeholder="Enter your email address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+      />
       <button className="form__button">Join waitlist</button>
-    </div>
+    </form>
   );
 };
 
